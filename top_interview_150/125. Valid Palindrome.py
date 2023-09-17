6a339e4707562977468579060badbb40db12bd1d
Mario <liang.chen829@gmail.com>
2023-09-16 21:40:10 -0700

    add two pointer solution for 125.Valid Palindrome

--- top_interview_150/125. Valid Palindrome.py
+++ top_interview_150/125. Valid Palindrome.py
@@ -8,3 +8,14 @@ class Solution:
             if char.isalnum():
                 pali += char
         return pali[:] == pali[::-1]
+    
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join([v for v in s if v.isalnum()]).lower()
+        i, j = 0, len(s)-1
+        while i < j:
+            if s[i] != s[j]: return False
+            i += 1
+            j -= 1
+        return True 
