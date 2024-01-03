ac633326aaac77c6b58a24b3dca2ccf3fb25e907
Mario <liang.chen829@gmail.com>
2024-01-02 21:34:48 -0800

    Add one more Solution to 433

--- LeetCode/433.Minimum_Genetic_Mutation/Mario/433.Minimum_Genetic_Mutation.py
+++ LeetCode/433.Minimum_Genetic_Mutation/Mario/433.Minimum_Genetic_Mutation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-class Solution:
+class Solution1:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
 
         queue = deque()
@@ -25,5 +25,39 @@ class Solution:
                     else:
                         queue.append(
                             [str1, [s for s in tmp if s != str1], count+1])
+
+        return -1
+
+
+# Another Solution
+class Solution2:
+    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+        if endGene not in bank:
+            return -1
+
+        bank.append(startGene)
+        pMap = collections.defaultdict(list)
+        for gene in bank:
+            for i in range(len(gene)):
+                pattern = gene[:i] + "*" + gene[i+1:]
+                pMap[pattern].append(gene)
+
+        q = deque([startGene])
+        visited = set([startGene])
+        res = 0
+
+        while q:
+            for i in range(len(q)):
+                gene = q.popleft()
+                if gene == endGene:
+                    return res
+
+                for j in range(len(gene)):
+                    pattern = gene[:j] + "*" + gene[j+1:]
+                    for nei in pMap[pattern]:
+                        if nei not in visited:
+                            visited.add(nei)
+                            q.append(nei)
+            res += 1
 
         return -1
