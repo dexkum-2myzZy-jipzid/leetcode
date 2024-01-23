081ae1e68a23f473caf16d3a5deb3e97025d88d6
Mario <liang.chen829@gmail.com>
2024-01-23 13:01:06 -0800

    Add solution for LeetCode problem 137 Single Number II

---
+++ LeetCode/137.Single_Number_II/Mario/137.Single_Number_II.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int cnt = 0;
+            for (int num : nums) {
+                cnt += (num >> i) & 1;
+            }
+            res |= (cnt % 3) << i;
+        }
+        return res;
+    }
+}
