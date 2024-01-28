35108222613e83cabba373568f93edfd098d8ba2
Mario <liang.chen829@gmail.com>
2024-01-28 15:00:50 -0800

    Add solution for LeetCode problem 72: Edit Distance

---
+++ LeetCode/72.Edit_Distance/Mario/72.Edit_Distance.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length(), m = word2.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        // init first row & column
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i < m + 1; i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                int plus = word1.charAt(i - 1) == word2.charAt(j - 1) ? 0 : 1;
+                dp[i][j] = Math.min(Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + plus);
+            }
+        }
+        return dp[n][m];
+    }
+}
