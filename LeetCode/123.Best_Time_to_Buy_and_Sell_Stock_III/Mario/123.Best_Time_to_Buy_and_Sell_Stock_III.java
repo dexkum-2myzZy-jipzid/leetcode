2a1e83877d5b79c96f38ae88b19df9d73680ba07
Mario <liang.chen829@gmail.com>
2024-01-28 20:34:05 -0800

    Add solution for LeetCode problem 123: Best Time to Buy and Sell Stock III

---
+++ LeetCode/123.Best_Time_to_Buy_and_Sell_Stock_III/Mario/123.Best_Time_to_Buy_and_Sell_Stock_III.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        int buy1 = -prices[0];
+        int sell1 = 0;
+        int buy2 = -prices[0];
+        int sell2 = 0;
+
+        for (int i = 1; i < n; i++) {
+            buy1 = Math.max(buy1, 0 - prices[i]);
+            sell1 = Math.max(sell1, buy1 + prices[i]);
+            buy2 = Math.max(buy2, sell1 - prices[i]);
+            sell2 = Math.max(sell2, buy2 + prices[i]);
+        }
+
+        return sell2;
+    }
+}
+
+// Easy to understand version
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][5];
+
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        dp[0][2] = 0;
+        dp[0][3] = -prices[0];
+        dp[0][4] = 0;
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i - 1][0];
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i][0] - prices[i]);
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
+            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
+            dp[i][4] = Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
+        }
+
+        return dp[n - 1][4];
+    }
+}
