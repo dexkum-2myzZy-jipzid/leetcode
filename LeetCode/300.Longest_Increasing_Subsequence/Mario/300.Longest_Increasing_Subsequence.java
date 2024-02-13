189cb183bdafc07199a93f297642c3b6763f7b50
Mario <liang.chen829@gmail.com>
2024-02-12 21:19:49 -0800

    Add binary search implementation for Longest Increasing Subsequence

--- LeetCode/300.Longest_Increasing_Subsequence/Mario/300.Longest_Increasing_Subsequence.java
+++ LeetCode/300.Longest_Increasing_Subsequence/Mario/300.Longest_Increasing_Subsequence.java
@@ -17,3 +17,32 @@ class Solution {
         return max;
     }
 }
+
+// binary search
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] top = new int[n];
+
+        int piles = 0;
+        for (int num : nums) {
+            int left = 0, right = piles;
+
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (top[mid] < num) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            if (left == piles) {
+                piles += 1;
+            }
+            top[left] = num;
+        }
+
+        return piles;
+    }
+}
