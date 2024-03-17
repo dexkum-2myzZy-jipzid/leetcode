6249d14ba4d8401a08c440cd4771425d367dae00
Mario <liang.chen829@gmail.com>
2024-03-17 02:32:29 -0700

    Refactor countSubstrings method to improve performance

--- LeetCode/3084.Count_Substrings_Starting_and_Ending_with_Given_Characterv/Mario/3084.Count_Substrings_Starting_and_Ending_with_Given_Characterv.java
+++ LeetCode/3084.Count_Substrings_Starting_and_Ending_with_Given_Characterv/Mario/3084.Count_Substrings_Starting_and_Ending_with_Given_Characterv.java
@@ -1,25 +1,13 @@
 class Solution {
     public long countSubstrings(String s, char c) {
         char[] chs = s.toCharArray();
-        int n = s.length();
 
-        int[] cnt = new int[n];
-
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (chs[i] == c) {
+        long count = 0;
+        for (char ch : chs) {
+            if (ch == c)
                 count += 1;
-                cnt[i] = count;
-            }
         }
 
-        long res = 0;
-        for (int i = 0; i < n; i++) {
-            if (cnt[i] > 0) {
-                res += (count - cnt[i]);
-            }
-        }
-        res += count;
-        return res;
+        return count * (count + 1) / 2;
     }
 }
