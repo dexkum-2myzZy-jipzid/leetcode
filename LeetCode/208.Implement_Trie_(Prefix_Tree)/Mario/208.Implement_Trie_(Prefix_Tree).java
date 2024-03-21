534cbd4541763a981b1ccd69982598b5d5a31374
Mario <liang.chen829@gmail.com>
2024-03-21 02:22:03 -0700

    Add another expression Trie implementation

--- LeetCode/208.Implement_Trie_(Prefix_Tree)/Mario/208.Implement_Trie_(Prefix_Tree).java
+++ LeetCode/208.Implement_Trie_(Prefix_Tree)/Mario/208.Implement_Trie_(Prefix_Tree).java
@@ -54,3 +54,86 @@ class Trie {
  * boolean param_2 = obj.search(word);
  * boolean param_3 = obj.startsWith(prefix);
  */
+
+// Different Expression
+class Trie {
+
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        char[] chs = word.toCharArray();
+        for (int i = 0; i < chs.length; i++) {
+            char ch = chs[i];
+            if (!node.containsKey(ch)) {
+                node.put(ch, new TrieNode());
+            }
+            node = node.get(ch);
+        }
+        node.setEnd();
+    }
+
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+
+    private TrieNode searchPrefix(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char curLetter = word.charAt(i);
+            if (node.containsKey(curLetter)) {
+                node = node.get(curLetter);
+            } else {
+                return null;
+            }
+        }
+        return node;
+    }
+}
+
+class TrieNode {
+    private TrieNode[] links;
+    private boolean isEnd;
+
+    public TrieNode() {
+        links = new TrieNode[26];
+    }
+
+    public boolean containsKey(char ch) {
+        return links[ch - 'a'] != null;
+    }
+
+    public TrieNode get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    public void put(char ch, TrieNode node) {
+        links[ch - 'a'] = node;
+    }
+
+    public void setEnd() {
+        isEnd = true;
+    }
+
+    public boolean isEnd() {
+        return isEnd;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
