7730ba9a2fb44b36f237ae16aa50517a759df450
Mario <liang.chen829@gmail.com>
2024-03-25 13:42:49 -0700

    Add segment tree solution for mostFrequentIDs

--- LeetCode/3092.Most_Frequent_IDs/Mario/3092.Most_Frequent_IDs.java
+++ LeetCode/3092.Most_Frequent_IDs/Mario/3092.Most_Frequent_IDs.java
@@ -21,3 +21,50 @@ class Solution {
         return res;
     }
 }
+
+// segment tree
+class Solution {
+    public long[] mostFrequentIDs(int[] nums, int[] freq) {
+        // value in nums map to index
+        Map<Integer, Integer> idxMap = new HashMap<>();
+
+        int index = 0;
+        for (int n : nums) {
+            Integer i = idxMap.get(n);
+            if (i == null) {
+                idxMap.put(n, index);
+                index += 1;
+            }
+        }
+
+        // System.out.println(idxMap);
+
+        // create segment tree
+        int n = idxMap.size();
+        long[] tree = new long[n * 2];
+
+        // iterate nums & freq
+        long[] res = new long[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i], fre = freq[i];
+            int idx = idxMap.get(num);
+            int j = idx + n;
+            tree[j] += fre;
+            // update segment tree
+            while (j / 2 >= 1) {
+                int k = j / 2;
+                if ((2 * k + 1) >= 2 * n) {
+                    tree[k] = tree[j];
+                } else {
+                    tree[k] = Math.max(tree[2 * k], tree[2 * k + 1]);
+                }
+                j = k;
+            }
+
+            // System.out.println(Arrays.toString(tree));
+            res[i] = tree[1];
+        }
+
+        return res;
+    }
+}
