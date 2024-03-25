70e8a8a70e5307a54fc3ee7cea36af6fb9783869
Mario <liang.chen829@gmail.com>
2024-03-24 20:40:36 -0700

    Add another solution for rotated sorted array

--- LeetCode/33.Search_in_Rotated_Sorted_Array/Mario/33.Search_in_Rotated_Sorted_Array.java
+++ LeetCode/33.Search_in_Rotated_Sorted_Array/Mario/33.Search_in_Rotated_Sorted_Array.java
@@ -26,3 +26,42 @@ class Solution {
         return res;
     }
 }
+
+// find pivot(most samllest point), then binary search two side
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+
+        // find smallest element - pivot
+        while (left <= right) {
+            int m = (left + right) / 2;
+            if (nums[m] > nums[n - 1]) {
+                left = m + 1;
+            } else {
+                right = m - 1;
+            }
+        }
+
+        // binary search left side, then right side
+        int res = binarySearch(nums, 0, left - 1, target);
+        if (res != -1)
+            return res;
+
+        return binarySearch(nums, left, n - 1, target);
+    }
+
+    private int binarySearch(int[] nums, int left, int right, int target) {
+        while (left <= right) {
+            int m = (left + right) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] > target) {
+                right = m - 1;
+            } else {
+                left = m + 1;
+            }
+        }
+        return -1;
+    }
+}
