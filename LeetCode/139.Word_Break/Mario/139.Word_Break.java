185b0dd56d93924035587340e314637a26132967
Mario <liang.chen829@gmail.com>
2024-04-01 22:37:50 -0700

    Add Trie implementation for word break

--- LeetCode/139.Word_Break/Mario/139.Word_Break.java
+++ LeetCode/139.Word_Break/Mario/139.Word_Break.java
@@ -15,3 +15,57 @@ class Solution {
         return dp[dp.length - 1];
     }
 }
+
+// Trie
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        // Trie store words
+        Trie t = new Trie();
+        for (String w : wordDict) {
+            char[] chs = w.toCharArray();
+            Trie node = t;
+            for (int i = 0; i < chs.length; i++) {
+                int index = chs[i] - 'a';
+                if (node.children[index] == null) {
+                    node.children[index] = new Trie();
+                }
+                node = node.children[index];
+            }
+            node.isEnd = true;
+        }
+
+        // search
+        boolean[] dp = new boolean[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            // dp[i-1] is true, it's necessary to continue
+            if (i == 0 || dp[i - 1]) {
+                Trie node = t;
+                for (int j = i; j < s.length(); j++) {
+                    int index = s.charAt(j) - 'a';
+                    // no word
+                    if (node.children[index] == null) {
+                        break;
+                    }
+
+                    node = node.children[index];
+                    // reach word end
+                    if (node.isEnd) {
+                        dp[j] = true;
+                    }
+                }
+            }
+        }
+
+        return dp[s.length() - 1];
+    }
+}
+
+class Trie {
+    public Trie[] children;
+    public boolean isEnd;
+
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+}
