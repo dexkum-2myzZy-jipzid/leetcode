be53c8a38a1e46e6ad1d80d7b1aec90f7a6506f2
Mario <liang.chen829@gmail.com>
2024-04-15 15:42:09 -0700

    Refactor solution for LeetCode problem 187: Repeated DNA Sequences to use Rabin-Karp algorithm

--- LeetCode/187.Repeated_DNA_Sequences/Mario/187.Repeated_DNA_Sequences.java
+++ LeetCode/187.Repeated_DNA_Sequences/Mario/187.Repeated_DNA_Sequences.java
@@ -16,3 +16,46 @@ class Solution {
 }
 
 // Rabin-Karp
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        // base value
+        int l = 10;
+        if (s.length() <= l)
+            return new ArrayList();
+        int a = 4, al = (int) Math.pow(4, l);
+
+        Map<Character, Integer> toInt = new HashMap() {
+            {
+                put('A', 0);
+                put('C', 1);
+                put('G', 2);
+                put('T', 3);
+            }
+        };
+
+        int[] nums = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            nums[i] = toInt.get(s.charAt(i));
+        }
+
+        int h = 0;
+        Set<Integer> seen = new HashSet<>();
+        Set<String> result = new HashSet<>();
+        for (int i = l - 1; i < s.length(); i++) {
+            if (i == l - 1) {
+                for (int j = 0; j < l; j++) {
+                    h = h * a + nums[j];
+                }
+            } else {
+                h = h * a - nums[i - l] * al + nums[i];
+            }
+
+            if (seen.contains(h)) {
+                result.add(s.substring(i - l + 1, i + 1));
+            }
+            seen.add(h);
+        }
+
+        return new ArrayList<>(result);
+    }
+}
