f6261d2f53634658f2198ed45ab32599466ac6af
Mario <liang.chen829@gmail.com>
2024-04-30 19:59:36 -0700

    Refactor lowestCommonAncestor method in 235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java

--- LeetCode/235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Mario/235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
+++ LeetCode/235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Mario/235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -10,20 +10,16 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null)
-            return null;
-        if ((p.val < root.val && q.val > root.val) || (p.val > root.val && q.val < root.val)) {
+        if (root == null) {
             return root;
-        } else if (p.val < root.val && q.val < root.val) {
+        }
+
+        if (root.val > p.val && root.val > q.val) {
             return lowestCommonAncestor(root.left, p, q);
-        } else if (p.val > root.val && q.val > root.val) {
+        } else if (root.val < p.val && root.val < q.val) {
             return lowestCommonAncestor(root.right, p, q);
-        } else if (p.val == root.val) {
-            return p;
-        } else if (q.val == root.val) {
-            return q;
         } else {
-            return null;
+            return root;
         }
     }
 }
