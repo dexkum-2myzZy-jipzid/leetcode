d8cc10285badaecb664f4a613c810904ebe5b29a
Mario <liang.chen829@gmail.com>
2024-05-09 23:58:57 -0700

    Add solution for LeetCode problem 786: K-th Smallest Prime Fraction

---
+++ LeetCode/786.K-th_Smallest_Prime_Fraction/Mario/786.K-th_Smallest_Prime_Fraction.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        // binary search, left = 0, right = 1.0
+        double left = 0, right = 1.0;
+        int n = arr.length;
+
+        // compare left and right,
+        while (left < right) {
+
+            double mid = (left + right) / 2.0;
+            int count = 0, j = 1;
+            double fraction = 0.0;
+            int numeratorIdx = 0, denominatorIdx = 0;
+            // double for loop count then num of fractions smaller than mid
+            for (int i = 0; i < n; i++) {
+                for (j = i + 1; j < n; j++) {
+                    if (arr[i] <= mid * arr[j]) {
+                        double tmp = (double) arr[i] / arr[j];
+                        if (tmp > fraction) {
+                            fraction = tmp;
+                            numeratorIdx = i;
+                            denominatorIdx = j;
+                        }
+                        break;
+                    }
+                }
+
+                count += n - j;
+            }
+
+            // if num is bigger than k, right = mid
+            // num is smaller than k, left = mid
+            // num == k, return new int[]{nums[i], nums[j]}
+            if (count == k) {
+                return new int[] { arr[numeratorIdx], arr[denominatorIdx] };
+            } else if (count > k) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+
+        }
+
+        return new int[] {};
+    }
+}
