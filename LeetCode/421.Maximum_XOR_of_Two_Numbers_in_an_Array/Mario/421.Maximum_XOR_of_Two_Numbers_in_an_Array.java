ade509da4f91f9030b352766889d8308a37e29dc
Mario <liang.chen829@gmail.com>
2024-05-22 13:27:03 -0700

    Add Trie-based solution for LeetCode problem 421: Maximum XOR of Two Numbers in an Array

--- LeetCode/421.Maximum_XOR_of_Two_Numbers_in_an_Array/Mario/421.Maximum_XOR_of_Two_Numbers_in_an_Array.java
+++ LeetCode/421.Maximum_XOR_of_Two_Numbers_in_an_Array/Mario/421.Maximum_XOR_of_Two_Numbers_in_an_Array.java
@@ -31,3 +31,52 @@ class Solution {
         return result;
     }
 }
+
+// using Trie to solve this leetcode
+class Solution {
+    public int findMaximumXOR(int[] nums) {
+        // get maximum binary length
+        int maxNum = 0;
+        for (int num : nums) {
+            maxNum = Math.max(maxNum, num);
+        }
+        int len = Integer.toBinaryString(maxNum).length();
+        int maxXor = 0;
+
+        // iterate element in nums
+        TrieNode root = new TrieNode();
+        for (int num : nums) {
+            TrieNode node = root, xorNode = root;
+            int curXor = 0;
+
+            for (int i = len - 1; i >= 0; i--) {
+                int bit = (num >> i) & 1;
+                int toggledBit = bit ^ 1;
+
+                if (node.children[bit] == null) {
+                    node.children[bit] = new TrieNode();
+                }
+                node = node.children[bit];
+
+                if (xorNode.children[toggledBit] != null) {
+                    curXor |= (1 << i);
+                    xorNode = xorNode.children[toggledBit];
+                } else {
+                    xorNode = xorNode.children[bit];
+                }
+            }
+
+            maxXor = Math.max(maxXor, curXor);
+        }
+
+        return maxXor;
+    }
+}
+
+class TrieNode {
+    TrieNode children[];
+
+    TrieNode() {
+        this.children = new TrieNode[2];
+    }
+}
