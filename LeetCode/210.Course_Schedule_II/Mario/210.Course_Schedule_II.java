33f3f6cc6312f414c36ebe2b0d4b9a9b3a12d115
Mario <liang.chen829@gmail.com>
2024-07-23 23:33:51 -0700

    Implement Topological Sort using DFS for Course Schedule II problem

--- LeetCode/210.Course_Schedule_II/Mario/210.Course_Schedule_II.java
+++ LeetCode/210.Course_Schedule_II/Mario/210.Course_Schedule_II.java
@@ -35,3 +35,70 @@ class Solution {
         return res.size() < numCourses ? new int[] {} : res.stream().mapToInt(Integer::intValue).toArray();
     }
 }
+
+// DFS
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        // create graph
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        Set<Integer> visited = new HashSet<>();
+        boolean[] sources = new boolean[numCourses];
+        Arrays.fill(sources, true);
+
+        for (int i = 0; i < numCourses; i++) {
+            adjList.put(i, new ArrayList<>());
+        }
+
+        // add node and find source
+        for (int[] p : prerequisites) {
+            int pre = p[1], post = p[0];
+            adjList.get(pre).add(post);
+            sources[post] = false;
+        }
+
+        // bfs
+        List<Integer> result = new ArrayList<>(numCourses);
+        for (int i = 0; i < numCourses; i++) {
+            if (sources[i]) {
+                boolean ok = dfs(i, adjList, visited, new HashSet<Integer>(), result);
+                if (!ok) {
+                    return new int[0];
+                }
+            }
+        }
+
+        // have cycle
+        if (result.size() < numCourses) {
+            return new int[0];
+        }
+
+        // Reverse the solution and put it into an array.
+        int[] ans = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            ans[i] = result.get(numCourses - 1 - i);
+        }
+        return ans;
+    }
+
+    private boolean dfs(int i, Map<Integer, List<Integer>> adjList, Set<Integer> visited, Set<Integer> stack,
+            List<Integer> result) {
+        visited.add(i);
+        stack.add(i);
+        for (int nei : adjList.get(i)) {
+            if (visited.contains(nei)) {
+                if (stack.contains(nei)) {
+                    return false;
+                }
+            } else {
+                boolean ok = dfs(nei, adjList, visited, stack, result);
+                if (!ok) {
+                    return false;
+                }
+            }
+        }
+        stack.remove(i);
+        result.add(i);
+        return true;
+    }
+
+}
