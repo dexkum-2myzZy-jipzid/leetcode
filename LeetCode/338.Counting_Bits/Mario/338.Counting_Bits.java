f046c70983746a27bfc2b884b5fef2a096be676d
Mario <liang.chen829@gmail.com>
2025-01-08 17:39:26 -0800

    Add solution for LeetCode problem 338: Counting Bits

--- LeetCode/338.Counting_Bits/Mario/338.Counting_Bits.java
+++ LeetCode/338.Counting_Bits/Mario/338.Counting_Bits.java
@@ -1,14 +1,9 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] result = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
-            int count = 0;
-            int num = i;
-            for (; num != 0; count++) {
-                num &= (num - 1);
-            }
-            result[i] = count;
+        int[] res = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            res[i] = res[i / 2] + i % 2;
         }
-        return result;
+        return res;
     }
 }
