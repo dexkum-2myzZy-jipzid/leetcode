ef6bef38ed1f0029561ce2c40d0938e5e3c4d8b1
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-05-06 21:55:44 -0700

    Add Python solution for LeetCode problem 189: Rotate Array

--- LeetCode/189.Rotate_Array/Mario/189.Rotate_Array.py
+++ LeetCode/189.Rotate_Array/Mario/189.Rotate_Array.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+
 # 189. Rotate Array
+# three reverse
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
@@ -8,6 +10,31 @@ class Solution:
         """
         n = len(nums)
         k = k % n
-        nums[:n-k] = nums[:n-k][::-1]
-        nums[n-k:] = nums[n-k:][::-1]
+        nums[: n - k] = nums[: n - k][::-1]
+        nums[n - k :] = nums[n - k :][::-1]
         nums[:] = nums[::-1]
+
+
+# 环状替换（Cyclic Replacements）
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+        if k == 0 or n == 0:
+            return
+
+        start, count = 0, 0
+        while count < n:
+            cur = start
+            cur_val = nums[start]
+            while True:
+                nxt = (cur + k) % n
+                nums[nxt], cur_val = cur_val, nums[nxt]
+                count += 1
+                cur = nxt
+                if cur == start:
+                    break
+            start += 1
