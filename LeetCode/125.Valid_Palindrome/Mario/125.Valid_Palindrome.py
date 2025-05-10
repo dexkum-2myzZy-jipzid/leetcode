8ce1e56477b86bc1df3608761ba8ae3512d82908
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-05-09 20:17:00 -0700

    Refactor solution for LeetCode problem 125: Valid Palindrome

--- LeetCode/125.Valid_Palindrome/Mario/125.Valid_Palindrome.py
+++ LeetCode/125.Valid_Palindrome/Mario/125.Valid_Palindrome.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 # 125. Valid Palindrome
 class Solution:
     def isPalindrome(self, s: str) -> bool:
@@ -8,14 +9,10 @@ class Solution:
             if char.isalnum():
                 pali += char
         return pali[:] == pali[::-1]
-    
+
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = ''.join([v for v in s if v.isalnum()]).lower()
-        i, j = 0, len(s)-1
-        while i < j:
-            if s[i] != s[j]: return False
-            i += 1
-            j -= 1
-        return True 
+        # s => lowercase, remove non-alph char
+        s = ("").join([c for c in s if c.isalnum()]).lower()
+        return s == s[::-1]
