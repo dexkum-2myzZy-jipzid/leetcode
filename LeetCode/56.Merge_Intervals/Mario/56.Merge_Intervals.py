2e465a8aa22b13bb7c32f9eaf30b9a143cc6edf7
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-05-13 16:31:24 -0700

    Add Python solution for LeetCode problem 57: Insert Interval

--- LeetCode/56.Merge_Intervals/Mario/56.Merge_Intervals.py
+++ LeetCode/56.Merge_Intervals/Mario/56.Merge_Intervals.py
@@ -16,3 +16,31 @@ class Solution:
                 res.append(e)
 
         return res
+
+
+class Solution:
+    def insert(
+        self, intervals: List[List[int]], newInterval: List[int]
+    ) -> List[List[int]]:
+        # iterate interval in intervals,
+        res = []
+        inserted = False
+        for cur in intervals:
+
+            # cur is the left side of new interval
+            if cur[1] < newInterval[0]:
+                res.append(cur)
+            # right side
+            elif cur[0] > newInterval[1]:
+                if not inserted:
+                    res.append(newInterval)
+                    inserted = True
+                res.append(cur)
+            else:
+                newInterval[0] = min(cur[0], newInterval[0])
+                newInterval[1] = max(cur[1], newInterval[1])
+
+        if not inserted:
+            res.append(newInterval)
+
+        return res
