a7c80cad122ea146929679a2bafd5a5a1f60892e
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-05-14 21:45:04 -0700

    Add Python solution for LeetCode problem 2: Add Two Numbers

---
+++ LeetCode/2.Add_Two_Numbers/Mario/2.Add_Two_Numbers.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummy = ListNode(-1)
+        node = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            val = carry
+
+            if l1:
+                val += l1.val
+                l1 = l1.next
+
+            if l2:
+                val += l2.val
+                l2 = l2.next
+
+            carry = val // 10
+            val %= 10
+
+            node.next = ListNode(val)
+            node = node.next
+
+        return dummy.next
