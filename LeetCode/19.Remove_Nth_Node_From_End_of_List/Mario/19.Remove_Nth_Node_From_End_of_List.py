a2164e2489f5f593e66128fdac92e4ad5920c65c
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-05-16 14:24:03 -0700

    Add another Solution to improve readability and handle edge cases

--- LeetCode/19.Remove_Nth_Node_From_End_of_List/Mario/19.Remove_Nth_Node_From_End_of_List.py
+++ LeetCode/19.Remove_Nth_Node_From_End_of_List/Mario/19.Remove_Nth_Node_From_End_of_List.py
@@ -47,3 +47,32 @@ class Solution:
         reverse(dummy)
 
         return dummy.next
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # edge case
+        if not head:
+            return head
+
+        dummy = ListNode(-1)
+        dummy.next = head
+        slow = fast = dummy
+
+        for _ in range(n + 1):
+            if not fast:  # this means n > the number of list nodes
+                return head
+            fast = fast.next
+
+        while fast:
+            slow = slow.next
+            fast = fast.next
+
+        slow.next = slow.next.next
+
+        return dummy.next
