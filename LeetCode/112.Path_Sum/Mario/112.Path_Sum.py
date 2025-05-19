78cfde8ac6d883af7c503514fc62483566855568
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-05-19 16:35:11 -0700

    Add Python solution for LeetCode problem 112: Path Sum

---
+++ LeetCode/112.Path_Sum/Mario/112.Path_Sum.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+
+        remaining = targetSum - root.val
+
+        if remaining == 0 and not root.left and not root.right:
+            return True
+
+        return self.hasPathSum(root.left, remaining) or self.hasPathSum(
+            root.right, remaining
+        )
