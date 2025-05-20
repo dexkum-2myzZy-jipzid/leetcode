346284a9a27b0c11a599b21681432ee90f40f16c
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-05-20 10:05:26 -0700

    Refactor solution for LeetCode problem 637: Average of Levels in Binary Tree to use BFS approach

--- LeetCode/637.Average_of_Levels_in_Binary_Tree/Mario/637.Average_of_Levels_in_Binary_Tree.py
+++ LeetCode/637.Average_of_Levels_in_Binary_Tree/Mario/637.Average_of_Levels_in_Binary_Tree.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,24 +8,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-	def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-		res = []
-		if not root:
-			return res
-		
-		level_list = [root]
-		while level_list:
-			array = []
-			sum_ = 0
-			count = len(level_list)
-			for node in level_list:
-				sum_ += node.val
-				if node.left:
-					array.append(node.left)
-				if node.right:
-					array.append(node.right)
-					
-			res.append(sum_/count)
-			level_list = array
-			
-		return res
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        # bfs
+        q = deque([root])
+        res = []
+
+        while q:
+            size = len(q)
+            sum0 = 0
+            for i in range(size):
+                cur = q.popleft()
+                sum0 += cur.val
+                if cur.left:
+                    q.append(cur.left)
+                if cur.right:
+                    q.append(cur.right)
+            res.append(sum0 / size)
+
+        return res
