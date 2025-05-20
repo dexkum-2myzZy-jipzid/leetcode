d97073e18762f5e29b9bf9c8000534023760f744
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-05-20 09:55:51 -0700

    Add another solution for LeetCode problem 199: Binary Tree Right Side View

--- LeetCode/199.Binary_Tree_Right_Side_View/Mario/199.Binary_Tree_Right_Side_View.py
+++ LeetCode/199.Binary_Tree_Right_Side_View/Mario/199.Binary_Tree_Right_Side_View.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,24 +8,50 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-	def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-		res = []
-		
-		if not root:
-			return res
-		
-		level = [root]
-		
-		while level:
-			rightMostValue = 0
-			array = []
-			for node in level:
-				rightMostValue = node.val
-				if node.left:
-					array.append(node.left)
-				if node.right:
-					array.append(node.right)
-			res.append(rightMostValue)
-			level = array
-			
-		return res
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        if not root:
+            return res
+
+        level = [root]
+
+        while level:
+            rightMostValue = 0
+            array = []
+            for node in level:
+                rightMostValue = node.val
+                if node.left:
+                    array.append(node.left)
+                if node.right:
+                    array.append(node.right)
+            res.append(rightMostValue)
+            level = array
+
+        return res
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def dfs(node, depth):
+            if not node:
+                return
+
+            if depth == len(res):
+                res.append(node.val)
+
+            # right first
+            dfs(node.right, depth + 1)
+            dfs(node.left, depth + 1)
+
+        dfs(root, 0)
+
+        return res
