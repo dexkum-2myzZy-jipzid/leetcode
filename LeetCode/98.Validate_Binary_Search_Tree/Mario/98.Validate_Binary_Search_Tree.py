cdc8ce579ff681dba480c4105f051126122bbe3e
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-05-20 11:42:04 -0700

    Refactor solution for LeetCode problem 98: Validate Binary Search Tree to use inorder traversal

--- LeetCode/98.Validate_Binary_Search_Tree/Mario/98.Validate_Binary_Search_Tree.py
+++ LeetCode/98.Validate_Binary_Search_Tree/Mario/98.Validate_Binary_Search_Tree.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -16,5 +17,35 @@ class Solution:
         if min >= root.val or max <= root.val:
             return False
         res = self.helper(root.left, min, root.val) and self.helper(
-            root.right, root.val, max)
+            root.right, root.val, max
+        )
         return res
+
+
+# inorder traverse solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        self.pre = None
+        self.res = True
+
+        def inorder(root):
+            # leaf or self.res is False
+            if not root or not self.res:
+                return
+
+            inorder(root.left)
+            if self.pre is not None:
+                if root.val <= self.pre:
+                    self.res = False
+            self.pre = root.val
+            inorder(root.right)
+
+        inorder(root)
+
+        return self.res
