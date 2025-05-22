46c40aa45a8a4d4b3032f9d1653a96436239ff59
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-05-22 15:16:08 -0700

    Add BFS solution for LeetCode problem 133: Clone Graph

--- LeetCode/133.Clone_Graph/Mario/133.Clone_Graph.py
+++ LeetCode/133.Clone_Graph/Mario/133.Clone_Graph.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 """
 # Definition for a Node.
 class Node:
@@ -11,8 +12,9 @@ class Node:
 from typing import Optional
 
 
+# dfs
 class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
         oldToNew = {}
 
         def dfs(node):
@@ -26,3 +28,29 @@ class Solution:
             return cp
 
         return dfs(node) if node else None
+
+
+# bfs
+class Solution:
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+        # create graph with adjacency list
+        # handle edge case
+        if not node:
+            return None
+
+        q = deque([node])
+        copy = Node(node.val)
+
+        dic = {node.val: copy}  # store copy nodes // val: node
+
+        while q:
+            cur = q.popleft()
+            clone = dic[cur.val]
+
+            for nei in cur.neighbors:
+                if nei.val not in dic:
+                    dic[nei.val] = Node(nei.val)
+                    q.append(nei)
+                clone.neighbors.append(dic[nei.val])
+
+        return copy
