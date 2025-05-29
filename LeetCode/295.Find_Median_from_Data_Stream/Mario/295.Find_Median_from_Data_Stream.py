fa8a285289430020f583070aee63f41ade2a4a6b
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-05-29 15:12:00 -0700

    Add solution for LeetCode problem 295: Find Median from Data Stream

---
+++ LeetCode/295.Find_Median_from_Data_Stream/Mario/295.Find_Median_from_Data_Stream.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import heapq
+
+
+class MedianFinder:
+    def __init__(self):
+        self.left = []  # 最大堆（用负数模拟）
+        self.right = []  # 最小堆
+
+    def addNum(self, num: int) -> None:
+        if not self.left or num <= -self.left[0]:
+            heapq.heappush(self.left, -num)
+        else:
+            heapq.heappush(self.right, num)
+
+        # 调整两堆平衡
+        if len(self.left) > len(self.right) + 1:
+            heapq.heappush(self.right, -heapq.heappop(self.left))
+        elif len(self.right) > len(self.left):
+            heapq.heappush(self.left, -heapq.heappop(self.right))
+
+    def findMedian(self) -> float:
+        if len(self.left) > len(self.right):
+            return -self.left[0]
+        else:
+            return (-self.left[0] + self.right[0]) / 2
