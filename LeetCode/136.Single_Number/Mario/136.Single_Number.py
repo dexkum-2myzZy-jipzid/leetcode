9b4968e3dce2f164acc17b7e0b9102ca9be948eb
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-05-29 16:00:51 -0700

    Add solution for LeetCode problem 136: Single Number

---
+++ LeetCode/136.Single_Number/Mario/136.Single_Number.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            res ^= num
+        return res
