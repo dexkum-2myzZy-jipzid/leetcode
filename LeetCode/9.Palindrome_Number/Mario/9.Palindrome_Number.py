5e949aa97dd8f22e799c9efa6dca05f684af18d0
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-05-29 20:42:09 -0700

    Add solution for LeetCode problem 9: Palindrome Number

---
+++ LeetCode/9.Palindrome_Number/Mario/9.Palindrome_Number.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        reverse_half = 0
+
+        # 12321 123 > 12
+        while x > reverse_half:
+            reverse_half = reverse_half * 10 + x % 10
+            x //= 10
+
+        return x == reverse_half or x == reverse_half // 10
