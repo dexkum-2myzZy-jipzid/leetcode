39a0ea64132b0284974b3cf60f023226a27a0a72
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-06-03 15:45:53 -0700

    Add multi-dimensional dynamic programming solution for LeetCode problem 123: Best Time to Buy and Sell Stock III

--- LeetCode/123.Best_Time_to_Buy_and_Sell_Stock_III/Mario/123.Best_Time_to_Buy_and_Sell_Stock_III.py
+++ LeetCode/123.Best_Time_to_Buy_and_Sell_Stock_III/Mario/123.Best_Time_to_Buy_and_Sell_Stock_III.py
@@ -68,3 +68,36 @@ class Solution:
             hold1 = max(hold1, -today)
 
         return sell2
+
+
+# multi dimensional dp
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        dp[i][k][0/1]: i: ith day, k:transactions times (k <= 2),  0 /1 : hold stock or not
+
+        dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
+        dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
+
+        """
+
+        n = len(prices)
+        k = 2
+
+        dp = [[[0, 0] for _ in range(k + 1)] for _ in range(n)]
+
+        for i in range(k + 1):
+            dp[0][i][1] = -prices[0]
+
+        for i in range(1, n):
+            for j in range(k, 0, -1):
+                cur = prices[i]
+                # no stock
+                dp[i][j][0] = max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i])
+
+                # hold stock
+                dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i])
+
+        # print(dp)
+
+        return dp[n - 1][k][0]
