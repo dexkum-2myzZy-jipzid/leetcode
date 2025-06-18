9427d52534da92fa9f60ac0a7f96229841625025
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-06-17 21:59:18 -0700

    Add binary search solution for LeetCode problem 778: Swim in Rising Water

--- LeetCode/778.Swim_in_Rising_Water/Mario/778.Swim_in_Rising_Water.py
+++ LeetCode/778.Swim_in_Rising_Water/Mario/778.Swim_in_Rising_Water.py
@@ -8,17 +8,18 @@ class Solution:
         n = len(grid)
 
         heap = [(grid[0][0], 0, 0)]
+        # mark grid[0][0] visited
+        grid[0][0] = -1
         DIRS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
         res = inf
 
         while heap:
             t, i, j = heapq.heappop(heap)
+            # print(f"i: {i} j:{j} t:{t}")
 
             if i == n - 1 and j == n - 1:
-                if t < res:
-                    res = t
-                continue
+                return t
 
             # no need to continue
             if res != inf and t >= res:
@@ -32,6 +33,50 @@ class Solution:
                     heapq.heappush(heap, (nxt_t, nx, ny))
 
         return res
+
+
+# binary search
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        # binary search
+        n = len(grid)
+
+        DIRS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        # bfs
+        def can_reach(x):
+            visited = [[False] * n for _ in range(n)]
+            q = deque([(0, 0)])
+
+            while q:
+                i, j = q.popleft()
+
+                if i == n - 1 and j == n - 1:
+                    return True
+
+                for dx, dy in DIRS:
+                    nx, ny = i + dx, j + dy
+                    if (
+                        0 <= nx < n
+                        and 0 <= ny < n
+                        and not visited[nx][ny]
+                        and x >= grid[nx][ny]
+                    ):
+                        visited[nx][ny] = True
+                        q.append((nx, ny))
+            return False
+
+        # binary search
+        left, right = max(grid[0][0], grid[n - 1][n - 1]), n * n - 1
+
+        while left < right:
+            mid = (left + right) // 2
+            if can_reach(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
 
 
 # dfs
