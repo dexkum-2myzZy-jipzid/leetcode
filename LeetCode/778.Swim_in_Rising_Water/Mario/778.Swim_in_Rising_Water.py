b3ecd0a4e6ad6ffd31a185ae551457ccc6fcbcac
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-06-17 18:46:02 -0700

    Add DFS solution for LeetCode problem 778: Swim in Rising Water

--- LeetCode/778.Swim_in_Rising_Water/Mario/778.Swim_in_Rising_Water.py
+++ LeetCode/778.Swim_in_Rising_Water/Mario/778.Swim_in_Rising_Water.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 
+# Dijkstra
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         # Dijkstra algo
@@ -29,5 +30,50 @@ class Solution:
                     nxt_t = max(grid[nx][ny], t)
                     grid[nx][ny] = -1
                     heapq.heappush(heap, (nxt_t, nx, ny))
+
+        return res
+
+
+# dfs
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        # val in grid is unique
+        n = len(grid)
+
+        res = inf
+        DIRS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        @cache
+        def dfs(i, j, t):
+            nonlocal res
+            # cell no need to continue
+            if (
+                i < 0
+                or i >= n
+                or j < 0
+                or j >= n
+                or grid[i][j] == -1
+                or grid[i][j] > res
+            ):
+                return inf
+
+            if i == n - 1 and j == n - 1:
+                max_val = max(t, grid[n - 1][n - 1])
+                if max_val < res:
+                    res = max_val
+                return max_val
+
+            val = grid[i][j]
+            if val > t:
+                t = val
+            grid[i][j] = -1
+            ans = inf
+            for dx, dy in DIRS:
+                nx, ny = i + dx, j + dy
+                ans = min(dfs(nx, ny, t), ans)
+            grid[i][j] = val
+            return ans
+
+        dfs(0, 0, 0)
 
         return res
