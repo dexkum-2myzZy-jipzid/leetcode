c907c064393aaee69c4ff0a944a0fd2c90f94373
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-07-08 22:28:54 -0700

    Add one more solution for LeetCode problem 983: Minimum Cost For Tickets

--- LeetCode/983.Minimum_Cost_For_Tickets/Mario/983.Minimum_Cost_For_Tickets.py
+++ LeetCode/983.Minimum_Cost_For_Tickets/Mario/983.Minimum_Cost_For_Tickets.py
@@ -20,14 +20,48 @@ class Solution:
         for i in range(1, last_day + 1):
             # ith has 3 options
             if i not in day_set:
-                dp[i] = dp[i-1]
+                dp[i] = dp[i - 1]
             else:
                 cost_1 = dp[i - 1] + costs[0]
                 cost_7 = dp[max(0, i - 7)] + costs[1]
                 cost_30 = dp[max(0, i - 30)] + costs[2]
                 dp[i] = min(cost_1, cost_7, cost_30)
 
-
         # print(dp)
 
         return dp[last_day]
+
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        # dp[i]
+
+        # dp[i][0]: buy 1 day pass
+        # dp[i][1]: buy 7 day pass
+        # dp[i][2]: buy 30 day pass
+
+        # reverse dp
+        # dp[i][0] = cost[0] + min(dp[i+1])
+        # dp[i][1] = find(days[i] + 6) + min(dp[x])
+        # dp[i][2] = find(days[i] + 29) + min(dp[x])
+
+        n = len(days)
+        dp = [[0] * 3 for _ in range(n + 1)]
+
+        for i in range(n - 1, -1, -1):
+            # buy 1-day pass
+            dp[i][0] = costs[0] + min(dp[i + 1])
+
+            # buy 7-day pass
+            nxt = days[i] + 6
+            j = bisect.bisect_right(days, nxt)
+            dp[i][1] = costs[1] + min(dp[j])
+
+            # buy 30-day pass
+            nxt30 = days[i] + 29
+            k = bisect.bisect_right(days, nxt30)
+            dp[i][2] = costs[2] + min(dp[k])
+
+        # print(dp)
+
+        return min(dp[0])
