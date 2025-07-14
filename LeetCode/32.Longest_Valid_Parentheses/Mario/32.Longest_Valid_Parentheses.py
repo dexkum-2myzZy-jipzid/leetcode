157d50b1c87e5910385b4123483729ae79e7096e
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-07-14 16:19:54 -0700

    add dp solution for LeetCode problem 32: Longest Valid Parentheses

--- LeetCode/32.Longest_Valid_Parentheses/Mario/32.Longest_Valid_Parentheses.py
+++ LeetCode/32.Longest_Valid_Parentheses/Mario/32.Longest_Valid_Parentheses.py
@@ -16,3 +16,27 @@ class Solution:
                 else:
                     res = max(res, i - stack[-1])
         return res
+
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        n = len(s)
+        if n == 0:
+            return 0
+
+        dp = [0] * n
+
+        # "xxx ()"  dp[i] = dp[i-2] + 2
+        # "xxx())"  dp[i] = s[i - dp[i-1] - 1] == "(", dp[i-2]+2
+
+        for i in range(1, n):
+            c = s[i]
+            if c == ")":
+                if s[i - 1] == "(":
+                    pre_valid_pra = dp[i - 2] if i - 2 >= 0 else 0
+                    dp[i] = pre_valid_pra + 2
+                elif i - dp[i - 1] - 1 >= 0 and s[i - dp[i - 1] - 1] == "(":
+                    dp[i] = dp[i - 1] + 2 + dp[i - dp[i - 1] - 2]
+
+        # print(dp)
+        return max(dp)
