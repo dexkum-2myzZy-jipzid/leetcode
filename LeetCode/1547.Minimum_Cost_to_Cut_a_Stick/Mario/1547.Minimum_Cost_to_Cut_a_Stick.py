27a2be3140494aa22c2c728758feb50e05c01a0a
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-07-17 22:44:44 -0700

    Add solution for LeetCode problem 1547: Minimum Cost to Cut a Stick

--- LeetCode/1547.Minimum_Cost_to_Cut_a_Stick/Mario/1547.Minimum_Cost_to_Cut_a_Stick.py
+++ LeetCode/1547.Minimum_Cost_to_Cut_a_Stick/Mario/1547.Minimum_Cost_to_Cut_a_Stick.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+
+
+class Solution:
+    def minCost(self, n: int, cuts: List[int]) -> int:
+        cuts = [0] + cuts + [n]
+        cuts.sort()
+
+        m = len(cuts)
+        dp = [[inf] * m for _ in range(m)]
+
+        for i in range(m - 1):
+            dp[i][i + 1] = 0
+
+        for i in range(2, m):
+            for l in range(m):
+                r = l + i
+                if r >= m:
+                    break
+                for k in range(l + 1, r):
+                    dp[l][r] = min(dp[l][r], dp[l][k] + dp[k][r] + cuts[r] - cuts[l])
+
+        return dp[0][m - 1]
