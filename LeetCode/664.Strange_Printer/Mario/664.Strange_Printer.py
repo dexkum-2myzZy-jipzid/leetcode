8a5e5ea2d6e171800725afbeb64dcb755cb035ee
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-08-04 15:03:29 -0700

    Refactor strangePrinter method in 664.Strange_Printer for improved readability and efficiency

--- LeetCode/664.Strange_Printer/Mario/664.Strange_Printer.py
+++ LeetCode/664.Strange_Printer/Mario/664.Strange_Printer.py
@@ -3,22 +3,17 @@
 
 class Solution:
     def strangePrinter(self, s: str) -> int:
-        # min turn
-        # convert s -> "aaabb" -> 'ab'
-
-        t = []
-
-        for i in range(len(s)):
-            if t and s[i] == t[-1]:
+        tmp = []
+        for i, c in enumerate(s):
+            if tmp and tmp[-1] == c:
                 continue
             else:
-                t.append(s[i])
-
-        s = "".join(t)
-        # print(s)
+                tmp.append(c)
+        s = "".join(tmp)
 
         n = len(s)
         dp = [[inf] * n for _ in range(n)]
+
         for i in range(n):
             dp[i][i] = 1
 
@@ -27,11 +22,14 @@ class Solution:
                 j = i + length - 1
                 if j >= n:
                     break
-                # merge segment
-                for k in range(i + 1, j + 1):
-                    if s[i] == s[k]:
-                        dp[i][j] = min(dp[i][j], dp[i][k - 1] + dp[k][j] - 1)
-                    else:
-                        dp[i][j] = min(dp[i][j], dp[i][k - 1] + dp[k][j])
+
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i][j] = min(dp[i][j], dp[i][j - 1])
+                else:
+                    for k in range(i, j):
+                        dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j])
+
+        # for r in dp:
+        #     print(r)
 
         return dp[0][n - 1]
