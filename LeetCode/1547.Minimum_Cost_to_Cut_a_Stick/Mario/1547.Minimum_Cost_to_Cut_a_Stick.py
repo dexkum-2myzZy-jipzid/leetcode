c1706e32f564100b7e938436cc42ea2749ec2a74
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-08-05 16:24:59 -0700

    Refactor solution for LeetCode problem 1547: Minimum Cost to Cut a Stick

--- LeetCode/1547.Minimum_Cost_to_Cut_a_Stick/Mario/1547.Minimum_Cost_to_Cut_a_Stick.py
+++ LeetCode/1547.Minimum_Cost_to_Cut_a_Stick/Mario/1547.Minimum_Cost_to_Cut_a_Stick.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python3
 
+from math import inf
+from typing import List
+
 
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
-        cuts = [0] + cuts + [n]
-        cuts.sort()
+        # stick n units
+        # cost = len(cur length of stick)
+        # total_cost = sum(cost)
+        # return minimum total cost
 
+        # n = min(n-1, 100) + 1
+        # dp[i][j] represents total cost of cuts[i:j]
+        # for k in (i, j), dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]) + (cuts[j] - cuts[i]))
+
+        cuts = [0] + sorted(cuts) + [n]
         m = len(cuts)
         dp = [[inf] * m for _ in range(m)]
 
+        # init dp
         for i in range(m - 1):
             dp[i][i + 1] = 0
 
-        for i in range(2, m):
-            for l in range(m):
-                r = l + i
-                if r >= m:
+        for length in range(3, m + 1):
+            for i in range(m):
+                j = i + length - 1
+                if j >= m:
                     break
-                for k in range(l + 1, r):
-                    dp[l][r] = min(dp[l][r], dp[l][k] + dp[k][r] + cuts[r] - cuts[l])
+                for k in range(i + 1, j):
+                    # [0, 1, 3] dp[0][1] dp[1][3]
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j] + cuts[j] - cuts[i])
+
+        # for r in dp:
+        #     print(r)
 
         return dp[0][m - 1]
