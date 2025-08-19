d30400ac07d7dfcd5c39a8ba7f540f7981217318
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-08-19 15:10:16 -0700

    Add solution for LeetCode problem 1046: Last Stone Weight

---
+++ LeetCode/1046.Last_Stone_Weight/Mario/1046.Last_Stone_Weight.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        heap = [-s for s in stones]
+        heapq.heapify(heap)
+
+        while len(heap) > 1:
+            y = heapq.heappop(heap)
+            x = heapq.heappop(heap)
+            diff = abs(y - x)
+            if diff > 0:
+                heapq.heappush(heap, -diff)
+
+        return 0 if not heap else -heapq.heappop(heap)
