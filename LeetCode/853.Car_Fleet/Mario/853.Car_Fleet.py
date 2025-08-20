f19977829496d4bce2331263c9e824fd81fe9a2f
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-08-20 10:21:28 -0700

    Add solution for LeetCode problem 853: Car Fleet

---
+++ LeetCode/853.Car_Fleet/Mario/853.Car_Fleet.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = sorted(zip(position, speed), reverse=True)
+        # print(cars)
+        stack = []
+        for p, s in cars:
+            t = (target - p) / s
+            if not stack or stack[-1] < t:
+                stack.append(t)
+        return len(stack)
