f4d4f8e1773d11dc3e47681a7abe4be35f9a426e
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-08-19 20:51:59 -0700

    Add iterative solution for LeetCode problem 104: Maximum Depth of Binary Tree

--- LeetCode/104.Maximum_Depth_of_Binary_Tree/Mario/104.Maximum_Depth_of_Binary_Tree.py
+++ LeetCode/104.Maximum_Depth_of_Binary_Tree/Mario/104.Maximum_Depth_of_Binary_Tree.py
@@ -12,3 +12,20 @@ class Solution:
         if not root:
             return 0
         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = []
+        if root:
+            stack.append((root, 1))
+
+        depth = 0
+        while stack:
+            node, cur_depth = stack.pop()
+            if node:
+                depth = max(depth, cur_depth)
+                stack.append((node.left, cur_depth + 1))
+                stack.append((node.right, cur_depth + 1))
+
+        return depth
