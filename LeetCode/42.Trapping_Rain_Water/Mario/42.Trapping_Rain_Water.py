ac31e76225f5e6e792aee497a2d4a8201dc20e09
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-08-27 18:54:53 -0700

    Add solution for LeetCode problem 42: Trapping Rain Water with two approaches

--- LeetCode/42.Trapping_Rain_Water/Mario/42.Trapping_Rain_Water.py
+++ LeetCode/42.Trapping_Rain_Water/Mario/42.Trapping_Rain_Water.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from typing import List
+
 
 class Solution:
     def trap(self, height: List[int]) -> int:
@@ -21,5 +23,41 @@ class Solution:
                 res += minH * (i - left - 1)
 
             stack.append(i)
+
+        return res
+
+
+# heap & bfs
+import heapq
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # heap + bfs
+        n = len(height)
+
+        # edge case
+        if n < 3:
+            return 0
+
+        heap = []
+        visited = [False] * n
+        for i in [0, n - 1]:
+            heapq.heappush(heap, (height[i], i))
+            visited[i] = True
+
+        res = 0
+        DIRS = [-1, 1]
+        while heap:
+            cur, i = heapq.heappop(heap)
+
+            for di in DIRS:
+                ni = di + i
+                if 0 <= ni < n and not visited[ni]:
+                    trap_water = max(0, cur - height[ni])
+                    res += trap_water
+
+                    visited[ni] = True
+                    heapq.heappush(heap, (max(cur, height[ni]), ni))
 
         return res
