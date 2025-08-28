593cc82955ff09d4c2614711b38eff8170209140
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-08-27 21:54:16 -0700

    Refactor UnionFind class methods for improved readability and efficiency

--- DataStructureAndAlgo/unionfind.py
+++ DataStructureAndAlgo/unionfind.py
@@ -5,22 +5,31 @@ class UnionFind:
     def __init__(self, n):
         self.parent = list(range(n))
         self.size = [1] * n
+        self.components = n
 
     def find(self, x):
-        while x != self.parent[x]:
-            self.parent[x], x = self.parent[self.parent[x]], self.parent[x]
-        return x
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
 
-    def union(self, a, b):
-        ra = self.find(a)
-        rb = self.find(b)
-        if ra == rb:
+    def union(self, x, y):
+        rx, ry = self.find(x), self.find(y)
+        if rx == ry:
             return False
-        if self.size[ra] < self.size[rb]:
-            ra, rb = rb, ra
-        self.parent[rb] = ra
-        self.size[ra] += self.size[rb]
+
+        if self.size[ry] > self.size[rx]:
+            rx, ry = ry, rx
+
+        self.size[rx] += self.size[ry]
+        self.parent[ry] = rx
+        self.components -= 1
         return True
 
-    def connected(self, a, b):
-        return self.find(a) == self.find(b)
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def get_size(self, x):
+        return self.size[self.find(x)]
+
+    def count_components(self):
+        return self.components
