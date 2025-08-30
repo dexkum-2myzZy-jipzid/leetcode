cac3ad31ea98d122820f2ebaf6728aba86d45a36
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-08-29 21:15:31 -0700

    Add MST- Prim algo to leetcode 1584

--- LeetCode/1584.Min_Cost_to_Connect_All_Points/Mario/1584.Min_Cost_to_Connect_All_Points.py
+++ LeetCode/1584.Min_Cost_to_Connect_All_Points/Mario/1584.Min_Cost_to_Connect_All_Points.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 import heapq
+from typing import List
+from math import inf
 
 
+# MST - Kruskal
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
         # check whether ith point in the MST
         inMST = set()
         # store distance bwteen point to MST
-        minDist = [math.inf] * n
+        minDist = [inf] * n
         # heap store vertex
         heap = [(0, 0)]
 
@@ -31,5 +34,38 @@ class Solution:
                     if dist < minDist[i]:
                         minDist[i] = dist
                         heapq.heappush(heap, (dist, i))
+
+        return res
+
+
+# MST - Prim
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        min_cost = [inf] * n
+        in_MST = [False] * n
+
+        # init
+        min_cost[0] = 0
+
+        res = 0
+        for _ in range(n):
+            u, best = -1, inf
+            for i in range(n):
+                if not in_MST[i] and min_cost[i] < best:
+                    best, u = min_cost[i], i
+
+            # add vertex u into MST
+            in_MST[u] = True
+            res += best
+
+            # relax: update all points which not in MST
+            x, y = points[u]
+            for v in range(n):
+                if not in_MST[v]:
+                    a, b = points[v]
+                    dis = abs(a - x) + abs(b - y)
+                    if dis < min_cost[v]:
+                        min_cost[v] = dis
 
         return res
