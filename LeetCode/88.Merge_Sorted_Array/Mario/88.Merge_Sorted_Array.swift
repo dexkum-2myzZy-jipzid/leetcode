53eaa0081d335d37c24a93ad72880be8ae949064
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-09-04 14:24:10 -0700

    Add new solution to 88. Merge Sorted Array

--- LeetCode/88.Merge_Sorted_Array/Mario/88.Merge_Sorted_Array.swift
+++ LeetCode/88.Merge_Sorted_Array/Mario/88.Merge_Sorted_Array.swift
@@ -1,27 +1,24 @@
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        // backforwad traversal this nums1
-        // put largest num first, then decrease
-        var j = m - 1
-        var k = n - 1
-        for i in (0 ..< (m + n)).reversed() {
-            // j & k > 0
-            var tmp = -1
-            if j >= 0, k >= 0 {
-                if nums1[j] >= nums2[k] {
-                    nums1[i] = nums1[j]
-                    j -= 1
-                } else {
-                    nums1[i] = nums2[k]
-                    k -= 1
-                }
-            } else if j >= 0 {
-                nums1[i] = nums1[j]
-                j -= 1
+        var p1 = m - 1
+        var p2 = n - 1
+        var p = m + n - 1
+
+        while p1 >= 0, p2 >= 0 {
+            if nums1[p1] > nums2[p2] {
+                nums1[p] = nums1[p1]
+                p1 -= 1
             } else {
-                nums1[i] = nums2[k]
-                k -= 1
+                nums1[p] = nums2[p2]
+                p2 -= 1
             }
+            p -= 1
+        }
+
+        while p2 >= 0 {
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
         }
     }
 }
