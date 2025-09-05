29bd4567356458190e88a27921d6e2b05e6c72c9
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-09-04 20:13:25 -0700

    Add second implementation of validWordAbbreviation function

--- LeetCode/408.Valid_Word_Abbreviation/Mario/408.Valid_Word_Abbreviation.swift
+++ LeetCode/408.Valid_Word_Abbreviation/Mario/408.Valid_Word_Abbreviation.swift
@@ -25,3 +25,36 @@ class Solution {
         return i == n && j == m
     }
 }
+
+class Solution {
+    func validWordAbbreviation(_ word: String, _ abbr: String) -> Bool {
+        let wordArr = Array(word), abbrArr = Array(abbr)
+        let n = wordArr.count, m = abbrArr.count
+        var i = 0, j = 0
+
+        while i < n && j < m {
+            if abbrArr[j].isLetter {
+                if wordArr[i] == abbrArr[j] {
+                    i += 1
+                    j += 1
+                } else {
+                    return false
+                }
+            } else if abbrArr[j].isNumber {
+                if abbrArr[j] == "0" {
+                    return false
+                }
+                var num = 0
+                while j < m && abbrArr[j].isNumber {
+                    num = num * 10 + Int(String(abbrArr[j]))!
+                    j += 1
+                }
+                i += num
+            } else {
+                return false
+            }
+        }
+
+        return i == n && j == m
+    }
+}
