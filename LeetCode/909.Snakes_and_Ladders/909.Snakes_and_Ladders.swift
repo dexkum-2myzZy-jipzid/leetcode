14201015b2764613d0813eed7caa1bd3d073e6a2
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-09-23 15:53:54 -0700

    Refactor index conversion logic in snakesAndLadders function for clarity

--- LeetCode/909.Snakes_and_Ladders/909.Snakes_and_Ladders.swift
+++ LeetCode/909.Snakes_and_Ladders/909.Snakes_and_Ladders.swift
@@ -1,42 +1,39 @@
 class Solution {
     func snakesAndLadders(_ board: [[Int]]) -> Int {
-        // if has a snake or ladder, move to dest
-        // [curr + 1, min(curr + 6, n2)]
-
         let n = board.count
 
-        // 1 -> (n-1, 0) 15 -> (n-3, 3) n = 6
-        func toPoint(_ label: Int) -> (Int, Int) {
-            let zeroBase = label - 1
-            let r = n - 1 - (zeroBase / n)
-            // get col
+        // 1 -> (n-1, 0)
+        func indexToPoint(_ index: Int) -> (Int, Int) {
+            let zeroBase = index - 1
             let rowFromBottom = zeroBase / n + 1
-            // left -> right
-            let c = rowFromBottom & 1 == 1 ? zeroBase % n : (n - 1 - zeroBase % n)
-            return (r, c)
+            let row = n - rowFromBottom
+            //
+            let col = rowFromBottom & 1 == 1 ? zeroBase % n : (n - 1 - zeroBase % n)
+
+            return (row, col)
         }
 
-        let target = n * n
-        var q = [1]
+        // print(indexToPoint(1))
+        // print(indexToPoint(15))
+        var q = [1], head = 0, step = 0
         var seen: Set<Int> = [1]
-        var head = 0, step = 0
 
         while head < q.count {
             let size = q.count - head
 
             for _ in 0 ..< size {
-                let label = q[head]
+                let curr = q[head]
                 head += 1
 
-                if label == target {
+                if curr == n * n {
                     return step
                 }
 
-                for i in 1 ... 6 {
-                    var next = label + i
-                    if next > target { break }
+                for add in 1 ... 6 {
+                    var next = curr + add
+                    if next > n * n { break }
 
-                    let (r, c) = toPoint(next)
+                    let (r, c) = indexToPoint(next)
                     if board[r][c] != -1 {
                         next = board[r][c]
                     }
@@ -47,6 +44,7 @@ class Solution {
                     }
                 }
             }
+
             step += 1
         }
 
