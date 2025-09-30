abf5e638cb307f5bb70d046071c8a0e9955d1c46
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-09-30 10:55:00 -0700

    Add alternative Swift solution for LeetCode problem 139. Word Break

--- LeetCode/139.Word_Break/139.Word_Break.swift
+++ LeetCode/139.Word_Break/139.Word_Break.swift
@@ -30,3 +30,28 @@ class Solution {
         return dp[n]
     }
 }
+
+class Solution {
+    func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
+        // dp[i] represent s[0..i] can be segmented into dic or not
+        let chs = Array(s)
+        let n = chs.count
+        let wordSet = Set(wordDict)
+
+        var dp = Array(repeating: false, count: n + 1)
+
+        // init dp, "" string
+        dp[0] = true
+
+        for right in 1 ... n {
+            for left in 0 ..< right {
+                if dp[left] && wordSet.contains(String(chs[left ..< right])) {
+                    dp[right] = true
+                    break
+                }
+            }
+        }
+
+        return dp[n]
+    }
+}
