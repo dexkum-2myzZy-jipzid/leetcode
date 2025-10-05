26566fc6624001c3a8a604b21622a6bf8ca19ea1
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-10-04 18:53:54 -0700

    Add second solution for LeetCode problem 32. Longest Valid Parentheses using dynamic programming

--- LeetCode/32.Longest_Valid_Parentheses/32.Longest_Valid_Parentheses.swift
+++ LeetCode/32.Longest_Valid_Parentheses/32.Longest_Valid_Parentheses.swift
@@ -19,3 +19,32 @@ class Solution {
         return res
     }
 }
+
+class Solution {
+    func longestValidParentheses(_ s: String) -> Int {
+        let chs = Array(s)
+        let n = chs.count
+
+        var dp = Array(repeating: 0, count: n)
+        var res = 0
+
+        for (i, ch) in chs.enumerated() {
+            guard i > 0, ch == ")" else { continue }
+
+            // ()
+            if chs[i - 1] == "(" {
+                dp[i] = (i - 2 >= 0 ? dp[i - 2] : 0) + 2
+            } else {
+                // (())
+                let j = i - dp[i - 1] - 1
+                if j >= 0 && chs[j] == "(" {
+                    dp[i] = dp[i - 1] + 2 + (j >= 1 ? dp[j - 1] : 0)
+                }
+            }
+
+            res = max(res, dp[i])
+        }
+
+        return res
+    }
+}
