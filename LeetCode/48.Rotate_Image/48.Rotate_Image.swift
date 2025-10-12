1a6662afdc81f47e071feaba058d15c338aeeec3
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-10-12 16:28:32 -0700

    Add Swift new Solution for 48.Rotate_Image

--- LeetCode/48.Rotate_Image/48.Rotate_Image.swift
+++ LeetCode/48.Rotate_Image/48.Rotate_Image.swift
@@ -16,3 +16,18 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    func rotate(_ matrix: inout [[Int]]) {
+        let n = matrix.count
+        guard n > 1 else { return }
+
+        // i: 0 ..< ceil(n/2),  j: 0 ..< floor(n/2)
+        for i in 0 ..< ((n + 1) / 2) {
+            for j in 0 ..< (n / 2) {
+                // (i, j) -> (j, n-1-i) -> (n-1-i, n-1-j) -> (n-1-j, i)
+                (matrix[i][j], matrix[n - 1 - j][i], matrix[n - 1 - i][n - 1 - j], matrix[j][n - 1 - i]) = (matrix[n - 1 - j][i], matrix[n - 1 - i][n - 1 - j], matrix[j][n - 1 - i], matrix[i][j])
+            }
+        }
+    }
+}
