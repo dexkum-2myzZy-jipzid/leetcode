4787f9a3875d5da5bbfc03cd4076e162873e49d7
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-10-20 22:49:09 -0700

    Add alternative solution using max heap for LeetCode problem 976. Largest Perimeter Triangle

--- LeetCode/976.Largest_Perimeter_Triangle/976.Largest_Perimeter_Triangle.py
+++ LeetCode/976.Largest_Perimeter_Triangle/976.Largest_Perimeter_Triangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import heapq
+
 
 class Solution:
     def largestPerimeter(self, nums: list[int]) -> int:
@@ -15,5 +17,33 @@ class Solution:
             a, b, c = nums[i - 2], nums[i - 1], nums[i]
             if a + b > c:
                 return a + b + c
+
+        return 0
+
+
+class Solution:
+    def largestPerimeter(self, nums: list[int]) -> int:
+        # heapify this nums,
+        # because i want to make it max heap, so negative all element in nums
+        # [2,1,2] => [-2, -1, -2]
+
+        # then pop 2 element, a, b
+        # c is top of element, heap[0]
+        # if b + c > a: it means form triangle, it larget perimeter
+        # return
+        # if not, push(-b) into heap,
+
+        heap = [-num for num in nums]
+        heapq.heapify(heap)
+
+        while len(heap) >= 3:
+            a = -heapq.heappop(heap)
+            b = -heapq.heappop(heap)
+            c = -heap[0]
+
+            if b + c > a:
+                return a + b + c
+            else:
+                heapq.heappush(heap, -b)
 
         return 0
