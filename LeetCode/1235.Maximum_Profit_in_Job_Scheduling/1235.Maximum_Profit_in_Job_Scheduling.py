ab921c5d834444804ffa9d06ceaa0d77d46c1957
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-11-02 09:53:38 -0800

    Add dp solution to 1235.Maximum_Profit_in_Job_Scheduling.

--- LeetCode/1235.Maximum_Profit_in_Job_Scheduling/1235.Maximum_Profit_in_Job_Scheduling.py
+++ LeetCode/1235.Maximum_Profit_in_Job_Scheduling/1235.Maximum_Profit_in_Job_Scheduling.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import bisect
+from functools import cache
+
 
 class Solution:
     def jobScheduling(
-        self, startTime: List[int], endTime: List[int], profit: List[int]
+        self, startTime: list[int], endTime: list[int], profit: list[int]
     ) -> int:
         # assume starttime is sorted
         n = len(startTime)
@@ -34,3 +37,34 @@ class Solution:
             return max(res, dfs(next_job[i]) + profits[i])
 
         return dfs(0)
+
+
+# dp
+class Solution2:
+    def jobScheduling(
+        self, startTime: list[int], endTime: list[int], profit: list[int]
+    ) -> int:
+
+        # get jobs
+        jobs = []
+        for s, e, p in zip(startTime, endTime, profit):
+            jobs.append((s, e, p))
+
+        # sort jobs by endtime
+        jobs.sort(key=lambda x: x[1])
+
+        n = len(jobs)
+        dp = [0] * n
+
+        ends = [e for _, e, _ in jobs]
+
+        for i, (s, e, p) in enumerate(jobs):
+            # find jobs which endtime <= s
+            j = bisect.bisect_right(ends, s) - 1
+            # take current j jobs
+            take = p + (dp[j] if j >= 0 else 0)
+            # not take current j jobs
+            skip = dp[i - 1] if i > 0 else 0
+            dp[i] = max(take, skip)
+
+        return dp[n - 1]
