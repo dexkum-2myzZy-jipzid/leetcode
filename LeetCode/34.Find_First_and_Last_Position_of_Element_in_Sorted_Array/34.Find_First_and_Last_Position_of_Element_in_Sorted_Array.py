54d64ebc7956ad56a4ed1e9ec6567f929454d630
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-11-10 12:59:25 -0800

    Refactor searchRange method to use bisect for improved performance and clarity

--- LeetCode/34.Find_First_and_Last_Position_of_Element_in_Sorted_Array/34.Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ LeetCode/34.Find_First_and_Last_Position_of_Element_in_Sorted_Array/34.Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+import bisect
+
 
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
+    def searchRange(self, nums: list[int], target: int) -> list[int]:
         n = len(nums)
         l, r = 0, n
 
@@ -29,3 +31,13 @@ class Solution:
 
         res.append(l - 1)
         return res
+
+
+class Solution2:
+    def searchRange(self, nums: list[int], target: int) -> list[int]:
+        start = bisect.bisect_left(nums, target)
+        if start == len(nums) or nums[start] != target:
+            return [-1, -1]
+
+        end = bisect.bisect_right(nums, target)
+        return [start, end - 1]
