a7d5f2c0a214f808b181848d8c012c1b10c30173
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-11-13 14:35:55 -0800

    Add new solution to 968. Binary Tree Cameras

--- LeetCode/968.Binary_Tree_Cameras/968.Binary_Tree_Cameras.py
+++ LeetCode/968.Binary_Tree_Cameras/968.Binary_Tree_Cameras.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
+from typing import Optional
+from functools import cache
+
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def minCameraCover(self, root: Optional[TreeNode]) -> int:
         # min num of cameras
@@ -37,3 +42,36 @@ class Solution:
         res = dfs(root)
 
         return min(res[1], res[2])
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution2:
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        cover = {None}  # means
+
+        def dfs(node: Optional[TreeNode], parent=None):
+            if node:
+                dfs(node.left, node)
+                dfs(node.right, node)
+
+                # which case should be installed
+                # 1. parent is None, node is uncovered
+                # 2. left node is uncovered
+                # 3. right node is uncovered
+                if (
+                    (parent is None and node not in cover)
+                    or (node.left not in cover)
+                    or (node.right not in cover)
+                ):
+                    self.res += 1
+
+                    cover.update({parent, node, node.left, node.right})
+
+        dfs(root)
+        return self.res
