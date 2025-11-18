4dee8e0f64c9e12291c10d531523a3a4881391e9
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-11-18 14:23:32 -0800

    Refactor searchMatrix method to use binary search for improved efficiency

--- LeetCode/74.Search_a_2D_Matrix/74.Search_a_2D_Matrix.py
+++ LeetCode/74.Search_a_2D_Matrix/74.Search_a_2D_Matrix.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 
+import bisect
+
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # row is non-decreasing order
+        # row[i][0] > row[i-1][-1]
         m, n = len(matrix), len(matrix[0])
 
-        i = 0
-        while i < m:
-            cur = matrix[i]
-            index = bisect.bisect_left(cur, target)
-            if index == n:
-                i += 1
-            elif cur[index] == target:
+        def get_idx(mid: int) -> tuple[int, int]:
+            return (mid // n, mid % n)
+
+        left, right = 0, m * n - 1
+        while left <= right:
+            mid = (left + right) // 2
+            i, j = get_idx(mid)
+            if matrix[i][j] == target:
                 return True
+            elif matrix[i][j] > target:
+                right = mid - 1
             else:
-                return False
+                left = mid + 1
 
         return False
