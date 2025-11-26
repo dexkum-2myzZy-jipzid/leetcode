c64d9f78bbdc46916e818c5b913cc153feeb141f
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-11-26 06:41:10 -0800

    Refactor singleNonDuplicate method for improved clarity and efficiency

--- LeetCode/540.Single_Element_in_a_Sorted_Array/540.Single_Element_in_a_Sorted_Array.py
+++ LeetCode/540.Single_Element_in_a_Sorted_Array/540.Single_Element_in_a_Sorted_Array.py
@@ -1,48 +1,20 @@
 #!/usr/bin/env python3
-
-import bisect
-
-
-class Solution:
-    def singleNonDuplicate(self, nums: List[int]) -> int:
-        # sorted array, exactly 2, only 1 elmenet once
-        # return once element
-        # [1,1,2,3,3,4,4,8,8] n = 9 left = 1, right = 8
-
-        n = len(nums)
-        if n == 1:
-            return nums[0]
-
-        left, right = nums[0], nums[-1] + 1
-        while left < right:
-            mid = (left + right) >> 1
-            idx = bisect.bisect_left(nums, mid)
-            if idx % 2 == 0:
-                left = mid + 1
-            else:
-                right = mid
-
-        return left - 1
+# -*- coding: utf-8 -*-
 
 
 class Solution:
-    def singleNonDuplicate(self, nums: List[int]) -> int:
-        # sorted array, exactly 2, only 1 elmenet once
-        # return once element
-        # [1,1,2,3,3,4,4,8,8] n = 9 left = 1, right = 8
+    def singleNonDuplicate(self, nums: list[int]) -> int:
+        left, right = 0, len(nums) - 1
 
-        n = len(nums)
-
-        left, right = 0, n - 1
         while left < right:
-            mid = (left + right) >> 1
-            # ensure mid is even
+            mid = (left + right) // 2
+
             if mid % 2 == 1:
                 mid -= 1
 
             if nums[mid] == nums[mid + 1]:
                 left = mid + 2
-            else:
+            else:  # nums[mid] < nums[mid+1]
                 right = mid
 
         return nums[left]
