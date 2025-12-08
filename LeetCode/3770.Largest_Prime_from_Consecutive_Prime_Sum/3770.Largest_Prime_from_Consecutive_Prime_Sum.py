16dc529928838d93bd689d978d72d1b57ceafa66
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-12-07 20:35:17 -0800

    Refactor largestPrime method for clarity and efficiency

--- LeetCode/3770.Largest_Prime_from_Consecutive_Prime_Sum/3770.Largest_Prime_from_Consecutive_Prime_Sum.py
+++ LeetCode/3770.Largest_Prime_from_Consecutive_Prime_Sum/3770.Largest_Prime_from_Consecutive_Prime_Sum.py
@@ -4,46 +4,27 @@
 
 class Solution:
     def largestPrime(self, n: int) -> int:
-        # prime num <= n
 
-        # get array hold prime start from 2
+        # 1. get array contains all prime number
+        is_prime = [True] * (n + 1)
+        is_prime[0] = is_prime[1] = False
 
-        def get_primes(n: int) -> list[bool]:
-            if n < 2:
-                return []
+        p = 2
+        while p * p <= n:
+            if is_prime[p]:
+                for i in range(p * p, n + 1, p):
+                    is_prime[i] = False
+            p += 1
 
-            is_prime = [True] * (n + 1)
-            is_prime[0] = is_prime[1] = False
+        primes = [i for i in range(2, n + 1) if is_prime[i]]
 
-            p = 2
-            while p * p <= n:
-                if is_prime[p]:
-                    for x in range(p * p, n + 1, p):
-                        is_prime[x] = False
-                p += 1
-
-            return is_prime
-
-        is_prime = get_primes(n)
-        primes = [i for i, x in enumerate(is_prime) if x]
-        primes_set = set(primes)
-
-        res = 0
-        if (not is_prime) or (not primes):
-            return res
-
-        # prefix_sum = []
-
-        prefix = []
+        res = total = 0
         for p in primes:
-            if not prefix:
-                prefix.append(p)
-            else:
-                prefix.append(prefix[-1] + p)
+            total += p
+            if total > n:
+                break
 
-            last = prefix[-1]
-
-            if last <= n and last in primes_set:
-                res = last
+            if is_prime[total]:
+                res = total
 
         return res
