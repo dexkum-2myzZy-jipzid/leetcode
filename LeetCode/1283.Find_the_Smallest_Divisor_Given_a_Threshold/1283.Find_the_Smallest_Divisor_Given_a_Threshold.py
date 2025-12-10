cae9be73f13a5563e74773822c9d87a6e21bda91
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-12-10 11:27:33 -0800

    Refactor smallestDivisor method for clarity and efficiency

--- LeetCode/1283.Find_the_Smallest_Divisor_Given_a_Threshold/1283.Find_the_Smallest_Divisor_Given_a_Threshold.py
+++ LeetCode/1283.Find_the_Smallest_Divisor_Given_a_Threshold/1283.Find_the_Smallest_Divisor_Given_a_Threshold.py
@@ -2,33 +2,24 @@
 # -*- coding: utf-8 -*-
 
 
-from math import ceil
-
-
 class Solution:
     def smallestDivisor(self, nums: list[int], threshold: int) -> int:
 
-        # main idea is binary search, left = 1, right = max(nums) + 1
+        # binary search to find smallest divisor
 
-        # mid = (left + right) // 2
-
-        # try iterate all every element in nums, x = sum(ceil(element / mid))
-
-        # if x > threshold, left = mid +1, else x <= threshold, right = mid
-
-        left, right = 1, max(nums) + 1
-
-        def helper(divisor: int) -> bool:
+        def get_sum(k: int) -> int:
             res = 0
             for num in nums:
-                res += ceil(num / divisor)
-            return res <= threshold
+                res += (num + k - 1) // k
+            return res
 
+        left, right = 1, max(nums)
         while left < right:
             mid = (left + right) // 2
-            if helper(mid):
-                right = mid
-            else:
+            if get_sum(mid) > threshold:
                 left = mid + 1
+            else:
+                right = mid
+            # print(f"left:{left}\tright:{right}")
 
         return left
