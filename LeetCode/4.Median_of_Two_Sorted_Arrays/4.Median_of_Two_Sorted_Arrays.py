fa23a7a87ad0b595f512d788722650aaf0f31227
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-12-11 23:29:54 -0800

    Refactor findMedianSortedArrays method for improved clarity and efficiency; streamline variable usage and enhance readability

--- LeetCode/4.Median_of_Two_Sorted_Arrays/4.Median_of_Two_Sorted_Arrays.py
+++ LeetCode/4.Median_of_Two_Sorted_Arrays/4.Median_of_Two_Sorted_Arrays.py
@@ -1,34 +1,40 @@
 #!/usr/bin/env python3
 
 
+from math import inf
+from typing import List
+
+
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # the number of left part is (n+m+1) // 2
-        if len(nums1) > len(nums2):
+        if len(nums1) > len(nums2):  # why swap
             nums1, nums2 = nums2, nums1
 
-        m, n = len(nums1), len(nums2)
+        m, n = len(nums1), len(nums2)  # 3, 3 total = 6, half = 3
+        total = m + n
+        half = (total + 1) // 2  # why add 1
 
-        left = (n + m + 1) // 2
+        left, right = 0, m
+        while left <= right:
+            i = (left + right) // 2
+            j = half - i
 
-        l, r = 0, m
-        while l <= r:
-            i = (l + r) // 2
-            j = left - i
-
-            nums1_left = nums1[i - 1] if i > 0 else -math.inf
-            nums1_right = nums1[i] if i < m else math.inf
-            nums2_left = nums2[j - 1] if j > 0 else -math.inf
-            nums2_right = nums2[j] if j < n else math.inf
+            nums1_left = nums1[i - 1] if i - 1 >= 0 else -inf
+            nums1_right = nums1[i] if i < m else inf
+            nums2_left = nums2[j - 1] if j - 1 >= 0 else -inf
+            nums2_right = nums2[j] if j < n else inf
 
             if nums1_left <= nums2_right and nums2_left <= nums1_right:
-                if (m + n) % 2 == 1:
-                    return max(nums1_left, nums2_left)
+                # odd
+                max_left = max(nums1_left, nums2_left)
+                if total % 2 == 1:
+                    return float(max_left)
                 else:
-                    return (
-                        max(nums1_left, nums2_left) + min(nums1_right, nums2_right)
-                    ) / 2.0
+                    # even
+                    min_right = min(nums1_right, nums2_right)
+                    return float(max_left + min_right) / 2.0
+
             elif nums1_left > nums2_right:
-                r = i - 1
+                right = i - 1
             else:
-                l = i + 1
+                left = i + 1
