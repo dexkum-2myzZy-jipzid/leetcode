fa519fb74ffd12f05711bec89b06dbfe6f526d62
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-12-12 23:05:52 -0800

    Add solution2 to 852. Peak_Index_in_a_Mountain_Array

--- LeetCode/852.Peak_Index_in_a_Mountain_Array/852.Peak_Index_in_a_Mountain_Array.py
+++ LeetCode/852.Peak_Index_in_a_Mountain_Array/852.Peak_Index_in_a_Mountain_Array.py
@@ -2,12 +2,31 @@
 
 
 class Solution:
-    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+    def peakIndexInMountainArray(self, arr: list[int]) -> int:
         n = len(arr)
         left, right = 0, n
         while left < right:
             mid = (left + right) >> 1
             if mid + 1 < n and arr[mid] < arr[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+
+        return left
+
+
+class Solution2:
+    def peakIndexInMountainArray(self, arr: list[int]) -> int:
+        left, right = 0, len(arr)
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if mid == len(arr) - 1:
+                right = mid
+                continue
+
+            if arr[mid] < arr[mid + 1]:
                 left = mid + 1
             else:
                 right = mid
