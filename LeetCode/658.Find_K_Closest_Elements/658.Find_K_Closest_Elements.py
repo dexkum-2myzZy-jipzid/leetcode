e2c1c9881984750a6add37f4ea1f27f5e6ffbe53
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-12-13 00:28:54 -0800

    Refactor Solution2's findClosestElements method for clarity and efficiency; streamline logic and enhance readability

--- LeetCode/658.Find_K_Closest_Elements/658.Find_K_Closest_Elements.py
+++ LeetCode/658.Find_K_Closest_Elements/658.Find_K_Closest_Elements.py
@@ -17,3 +17,25 @@ class Solution:
                 right = mid
 
         return arr[left : left + k]
+
+
+class Solution2:
+    def findClosestElements(self, arr: list[int], k: int, x: int) -> list[int]:
+
+        # a[mid],a[mid+1],....,a[mid+k-1]
+        #        a[mid+1],.....,a[mid+k-1], a[mid+k]
+        # [a[mid]...  x. ... a[mid+k]]
+        # x - a[mid] < a[mid+k] - x
+
+        n = len(arr)
+
+        left, right = 0, n - k - 1
+        while left <= right:
+            mid = (left + right) // 2
+
+            if x - arr[mid] <= arr[mid + k] - x:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return arr[left : left + k]
