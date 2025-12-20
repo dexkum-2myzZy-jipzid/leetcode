0fe622038dc10bc67fa385dad721263b841254b2
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-12-19 19:52:28 -0800

    Refactor Solution2 in 3583. Count Special Triplets to improve clarity and efficiency

--- LeetCode/3583.Count_Special_Triplets/3583.Count_Special_Triplets.py
+++ LeetCode/3583.Count_Special_Triplets/3583.Count_Special_Triplets.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from collections import defaultdict
+from collections import defaultdict, Counter
 import bisect
 
 
@@ -35,5 +35,30 @@ class Solution:
                     i_count = (idx - 1) if num == 0 else idx
                     k_count = len(array) - idx
                     result = (result + i_count * k_count) % MOD
+
+        return result
+
+
+class Solution2:
+    def specialTriplets(self, nums: list[int]) -> int:
+        MOD = 10**9 + 7
+        n = len(nums)
+        if n < 3:
+            return 0
+
+        right_count = Counter(nums)
+        left_count = Counter()
+
+        result = 0
+        for j, num in enumerate(nums):
+            double = 2 * num
+            right_count[num] -= 1
+
+            i_count = left_count[double]
+            k_count = right_count[double]
+
+            result = (result + i_count * k_count) % MOD
+
+            left_count[num] += 1
 
         return result
