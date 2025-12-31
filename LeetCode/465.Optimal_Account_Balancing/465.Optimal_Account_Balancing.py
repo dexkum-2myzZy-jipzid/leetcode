f9dd413a028555b6b34da24c05ab3214dac0e3ca
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2025-12-30 17:49:13 -0800

    Refactor minTransfers method for improved readability and efficiency

--- LeetCode/465.Optimal_Account_Balancing/465.Optimal_Account_Balancing.py
+++ LeetCode/465.Optimal_Account_Balancing/465.Optimal_Account_Balancing.py
@@ -1,36 +1,40 @@
 #!/usr/bin/env python3
 
+from collections import Counter
+from math import inf
+
 
 class Solution:
-    def minTransfers(self, transactions: List[List[int]]) -> int:
-        # backtrack
-        balance = defaultdict(int)
+    def minTransfers(self, transactions: list[list[int]]) -> int:
+
+        balance = Counter()
         for f, t, a in transactions:
             balance[f] -= a
             balance[t] += a
 
-        debts = [x for x in balance.values() if x != 0]
+        debts = [v for v in balance.values() if v != 0]
         n = len(debts)
-        # print(debts)
-        res = inf
 
-        def dfs(i):
-            if i == len(debts):
+        def dfs(i: int) -> int:
+            # exit
+            if i == n:
                 return 0
 
+            # do nothing
             if debts[i] == 0:
                 return dfs(i + 1)
 
-            cnt = inf
+            res = inf
             for j in range(i + 1, n):
                 if debts[i] * debts[j] < 0:
+
                     debts[j] += debts[i]
-                    cnt = min(cnt, dfs(i + 1) + 1)
+                    res = min(res, dfs(i + 1) + 1)
                     debts[j] -= debts[i]
 
                     if debts[j] + debts[i] == 0:
                         break
 
-            return cnt if cnt != inf else 0
+            return res
 
         return dfs(0)
