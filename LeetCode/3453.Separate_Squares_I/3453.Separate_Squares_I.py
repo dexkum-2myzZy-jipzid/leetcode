c59a99258bdb27778003b7c378927ed642de6ade
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2026-01-02 15:34:40 -0800

    Add implementation for Solution2 in Separate Squares I

--- LeetCode/3453.Separate_Squares_I/3453.Separate_Squares_I.py
+++ LeetCode/3453.Separate_Squares_I/3453.Separate_Squares_I.py
@@ -38,3 +38,36 @@ class Solution:
             # print(f"left:{left}\tright:{right}")
 
         return left
+
+
+class Solution2:
+    def separateSquares(self, squares: list[list[int]]) -> float:
+
+        total = sum(l * l for _, _, l in squares)
+        target = total / 2
+
+        events = []
+        for x, y, l in squares:
+            events.append((y, l))
+            events.append((y + l, -l))
+
+        events.sort()
+
+        curr_area = 0.0
+        curr_width = 0.0
+        last_y = squares[0][0]
+
+        for y, delta in events:
+
+            if y > last_y:
+                segment = curr_width * (y - last_y)
+                if curr_area + segment >= target:
+                    needed = target - curr_area
+                    return last_y + (needed / curr_width)
+
+                curr_area += segment
+
+            last_y = y
+            curr_width += delta
+
+        return float(last_y)
