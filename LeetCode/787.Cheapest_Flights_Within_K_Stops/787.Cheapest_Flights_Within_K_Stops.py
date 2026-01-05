821a4ee81e7290049f052a3aece07a21a04206da
dexkum-2myzZy-jipzid <liang.chen829@gmail.com>
2026-01-04 17:07:24 -0800

    Add implementation for Solution3 in Cheapest Flights Within K Stops

--- LeetCode/787.Cheapest_Flights_Within_K_Stops/787.Cheapest_Flights_Within_K_Stops.py
+++ LeetCode/787.Cheapest_Flights_Within_K_Stops/787.Cheapest_Flights_Within_K_Stops.py
@@ -72,3 +72,34 @@ class Solution2:
                     heapq.heappush(heap, (nprice, nei, nstops))
 
         return -1
+
+
+class Solution3:
+    def findCheapestPrice(
+        self, n: int, flights: list[list[int]], src: int, dst: int, k: int
+    ) -> int:
+        graph = defaultdict(list)
+        for v, u, p in flights:
+            graph[v].append((u, p))
+
+        heap = [(0, 0, src)]
+        best = [inf] * n
+        best[src] = 0
+
+        while heap:
+            price, stops, curr = heapq.heappop(heap)
+
+            if curr == dst:
+                return price
+
+            # more than k stops, cut off branch
+            if stops > k or stops > best[curr]:
+                continue
+
+            best[curr] = stops
+
+            for nei, p in graph[curr]:
+                nprice = price + p
+                heapq.heappush(heap, (nprice, stops + 1, nei))
+
+        return -1
